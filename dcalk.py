204242f89ccccc29b56aa858486480a3b06db79c
HakDrawer770 <53626462+HakDrawer770@users.noreply.github.com>
2019-08-25 18:28:24 +0300

    Create dcalk.py

---
+++ dcalk.py
@@ -0,0 +1,16 @@
+what = ("Что делаем (+, -, *, /):"))
+a = float(input("Введите первое число: "))
+b = float(input("Введите второе число: "))
+if what == "+":
+  c = a = b
+  print(c)
+elif what == "-":
+  c = a - b
+  print(c)
+elif what == "*":
+  c = a * b
+elif what == "/":
+  c = a / b
+  print(c)
+else:
+  print("Выбрана неверная операция!")
